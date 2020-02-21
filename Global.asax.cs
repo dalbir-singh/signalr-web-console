d54ae26d76dfd00a1b34740305580b69b080de21
Dalbir Burhm <dalbir.burhm@gmail.com>
2020-02-21 23:46:52 +0000

    N/A

--- Global.asax.cs
+++ Global.asax.cs
@@ -9,11 +9,8 @@ namespace SignalR.WebConsole
 {
     // a digital comment
     // a remote comment
-<<<<<<< HEAD
     // another REMOTE comment
-=======
     // a LOCAL comment
->>>>>>> local comments
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
